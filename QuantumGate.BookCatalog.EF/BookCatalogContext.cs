6bdf651b1b84c9670b1e399393f8cdaaa84f2c40
April John Maraat <aj.maraat@promax.com>
2024-01-30 16:55:39 +0800

    Book Catalog init

---
+++ QuantumGate.BookCatalog.EF/BookCatalogContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuantumGate.BookCatalog.Models;
+using System;
+
+namespace QuantumGate.BookCatalog.EF
+{
+    public class BookCatalogContext : DbContext
+    {
+        public BookCatalogContext(DbContextOptions<BookCatalogContext> options) : base(options) { }
+        public virtual DbSet<Book> Books { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.EnableSensitiveDataLogging(false);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
