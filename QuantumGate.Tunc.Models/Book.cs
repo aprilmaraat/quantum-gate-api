add9d72e512d627ed7a9d1c43c2e24d93c6af972
April John Maraat <aj.maraat@promax.com>
2024-01-30 17:06:48 +0800

    Added data models.

---
+++ QuantumGate.Tunc.Models/Book.cs
@@ -0,0 +1,22 @@
+﻿namespace QuantumGate.BookCatalog.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime PublishDateUtc { get; set; }
+        public Category Category { get; set; }
+
+        public Book() 
+        {
+            Id = 0;
+            CategoryId = 0;
+            Title = string.Empty;
+            Description = string.Empty;
+            PublishDateUtc = DateTime.MinValue;
+            Category = new Category();
+        }
+    }
+}
---
+++ QuantumGate.Tunc.Models/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace QuantumGate.BookCatalog.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Category() 
+        {
+            Id = 0;
+            Name = string.Empty;
+        }
+    }
+}
