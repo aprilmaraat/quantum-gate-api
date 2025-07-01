8a29b08a619153d0821c1475436cf8c6d045b107
AJ <aj.maraat@3cloudsolutions.com>
2025-07-01 16:26:16 +0800

    Fix attempt on QuantumGate.Auth.EF.StartupEmulator to support migration execution.

---
+++ QuantumGate.Auth.EF.StartupEmulator/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Configuration.FileExtensions;
+using Microsoft.Extensions.Configuration.Json;
+using System.IO;
+
+namespace QuantumGate.Auth.EF.StartupEmulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public class QuantumGateAuthDbContextFactory : IDesignTimeDbContextFactory<QuantumGateAuthDbContext>
+        {
+            private const string migrationOptionsFileName = "MigrationOptions.json";
+            /// <summary>
+            /// Method to create MigrationOptions.json file (if not created) 
+            /// that should contain the connection string for Quarto.Auth.Master
+            /// </summary>
+            /// <param name="args"></param>
+            /// <returns></returns>
+            public QuantumGateAuthDbContext CreateDbContext(string[] args)
+            {
+                while (!File.Exists(migrationOptionsFileName))
+                {
+                    Console.WriteLine("Please type in a connection string to your Quarto.Master Database:");
+
+                    string inputString = Console.ReadLine();
+
+                    File.WriteAllText(migrationOptionsFileName,
+                        $@"{{
+  ""ConnectionStrings"": {{
+    ""Master"": ""{inputString.Replace("\\", "\\\\").Replace("\"", "\\\"")}""
+  }}
+}}");
+                }
+
+                IConfigurationBuilder configurationBuilder = new ConfigurationBuilder()
+                    .SetBasePath(Environment.CurrentDirectory)
+                    .AddJsonFile(migrationOptionsFileName);
+
+                IConfigurationRoot configRoot = configurationBuilder.Build();
+
+                args = new string[] { configRoot.GetConnectionString("Master") };
+
+                if (args == null || args.Length < 1)
+                    throw new ArgumentNullException(nameof(args)
+                        , "No arguments passed, the first argument must be the connectionString for the database");
+
+                string connectionString = args[0];
+                var optionsBuilder = new DbContextOptionsBuilder<QuantumGateAuthDbContext>();
+                Console.WriteLine($"Using connection string: {connectionString}");
+                optionsBuilder.UseSqlServer(connectionString);
+
+                return new QuantumGateAuthDbContext(optionsBuilder.Options);
+            }
+        }
+    }
+}
--- QuantumGateAPI/Services/Interfaces/IUserDataService.cs
+++ QuantumGateAPI/Services/Interfaces/IUserDataService.cs
@@ -1,9 +1,11 @@
 ﻿using QuantumGate.Auth.Models;
+using QuantumGateAPI.Utilities.Helper;
 
 namespace QuantumGateAPI.Services
 {
     public interface IUserDataService
     {
+        Task<PagedResult<UserData>> GetUserDataAsync(int pageNumber, int pageSize);
         Task<UserData> GetUserByIdAsync(Guid userId);
     }
 }
